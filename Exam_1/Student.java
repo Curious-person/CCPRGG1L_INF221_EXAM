2d2bc349b6a70c5d796288011dc734b2c232f7c1
John Kevin Abgao <111432788+Curious-person@users.noreply.github.com>
2022-09-05 09:50:11 +0800

    Add files via upload

---
+++ Exam_1/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+    String surname;
+    String firstName;
+    char MiddleInitial;
+    String dateofBirth;
+    int studentNumber;
+    String studentEmailAddress;
+    boolean iAmAwesome;
+    
+    public void sayMyStudentNumber(){
+        System.out.println(studentNumber);
+    }
+    public void sayMyEmailAddress(){
+        System.out.println(studentEmailAddress);
+    }
+    public void iAmAwesome(){
+        System.out.println(iAmAwesome);
+    }
+}
